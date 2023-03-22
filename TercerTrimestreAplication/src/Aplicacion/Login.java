ec92066453921d7ba15b20b3f84ff575867a8e41
BubbleDH <jorge.nieto35@gmail.com>
2023-03-22 19:25:47 +0100

    Nuevas ventanas de envento

--- TercerTrimestreAplication/src/Aplicacion/Login.java
+++ TercerTrimestreAplication/src/Aplicacion/Login.java
@@ -14,6 +14,10 @@ import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import java.awt.SystemColor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Login extends JFrame {
 
@@ -54,47 +58,72 @@ public class Login extends JFrame {
 		
 		JButton btnNewButton = new JButton("Conectate");
 		btnNewButton.addActionListener(e -> {
-				if(!passwordField.getText().isEmpty() && !textField.getText().isEmpty()) {
-					VentanaBienvenida frame = new VentanaBienvenida();
-					frame.setVisible(true);
-					loginV.setVisible(false);
-				}else if(textField.getText().isEmpty()){
-					JOptionPane.showMessageDialog(contentPane,"Usuario vacio","ERROR",JOptionPane.WARNING_MESSAGE);
+				if(passwordField.getText().isEmpty() && textField.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(contentPane,"Por favor, introduce los datos del login","Formulario vacio",JOptionPane.ERROR_MESSAGE);
 					textField.setForeground(Color.BLACK);
 					textField.setBackground(Color.RED);
-				}else {
-					JOptionPane.showMessageDialog(contentPane,"Campo de contraseña vacio","ERROR",JOptionPane.WARNING_MESSAGE);
 					passwordField.setForeground(Color.BLACK);
 					passwordField.setBackground(Color.RED);
+				}else {
+					if(!passwordField.getText().isEmpty() && !textField.getText().isEmpty()) {
+						VentanaBienvenida frame = new VentanaBienvenida();
+						frame.setVisible(true);
+						loginV.setVisible(false);
+					}else if(textField.getText().isEmpty()){
+						JOptionPane.showMessageDialog(contentPane,"Introduce un usuario","Usuario vacio",JOptionPane.WARNING_MESSAGE);
+						textField.setForeground(Color.BLACK);
+						textField.setBackground(Color.RED);
+					}else {
+						JOptionPane.showMessageDialog(contentPane,"Introduce una contraseña","Contraseña vacia",JOptionPane.WARNING_MESSAGE);
+						passwordField.setForeground(Color.BLACK);
+						passwordField.setBackground(Color.RED);
+					}
 				}
 			
 		});
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Mostrar contraseña");
+		rdbtnNewRadioButton.setOpaque(false);
+		rdbtnNewRadioButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (rdbtnNewRadioButton.isSelected()==true) {
+					passwordField.setEchoChar((char) 0);
+					}else {
+					passwordField.setEchoChar('●');      
+					} 
+			}
+		});
+		rdbtnNewRadioButton.setForeground(Color.RED);
+		rdbtnNewRadioButton.setBackground(SystemColor.desktop);
+		rdbtnNewRadioButton.setBounds(228, 199, 140, 23);
+		contentPane.add(rdbtnNewRadioButton);
 		btnNewButton.setForeground(Color.BLACK);
 		btnNewButton.setBackground(Color.RED);
-		btnNewButton.setBounds(184, 264, 89, 23);
+		btnNewButton.setBounds(184, 264, 113, 23);
 		contentPane.add(btnNewButton);
 		
 		textField = new JTextField();
 		textField.setForeground(Color.RED);
 		textField.setBackground(Color.BLACK);
-		textField.setBounds(228, 105, 86, 20);
+		textField.setBounds(228, 105, 140, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		passwordField = new JPasswordField();
 		passwordField.setForeground(Color.RED);
 		passwordField.setBackground(Color.BLACK);
-		passwordField.setBounds(228, 163, 86, 20);
+		passwordField.setBounds(228, 163, 140, 20);
 		contentPane.add(passwordField);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
 		lblNewLabel.setForeground(Color.RED);
-		lblNewLabel.setBounds(131, 108, 46, 14);
+		lblNewLabel.setBounds(119, 108, 58, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contraseña");
 		lblNewLabel_1.setForeground(Color.RED);
-		lblNewLabel_1.setBounds(131, 166, 63, 14);
+		lblNewLabel_1.setBounds(119, 166, 75, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
--- TercerTrimestreAplication/src/Aplicacion/VentanaPrincipal.java
+++ TercerTrimestreAplication/src/Aplicacion/VentanaPrincipal.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -13,8 +15,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JToggleButton;
 
 public class VentanaPrincipal {
 
@@ -64,6 +68,24 @@ public class VentanaPrincipal {
 		frmRegistroBetaDiablo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRegistroBetaDiablo.getContentPane().setLayout(null);
 		
+		JRadioButton rdbtnMostrarContrasea = new JRadioButton("Mostrar Contraseña");
+		rdbtnMostrarContrasea.setForeground(Color.RED);
+		rdbtnMostrarContrasea.setOpaque(false);
+		rdbtnMostrarContrasea.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (rdbtnMostrarContrasea.isSelected()==true) {
+					passwordField.setEchoChar((char) 0);
+					passwordField_1.setEchoChar((char) 0);
+					}else {
+					passwordField.setEchoChar('●');    
+					passwordField_1.setEchoChar('●');  
+					} 
+			}
+		});
+		rdbtnMostrarContrasea.setBounds(272, 206, 156, 23);
+		frmRegistroBetaDiablo.getContentPane().add(rdbtnMostrarContrasea);
+		
 		JLabel lblNewLabel = new JLabel("REGISTRTATE EN LA BETA");
 		lblNewLabel.setBounds(142, 11, 224, 14);
 		lblNewLabel.setBackground(Color.WHITE);
@@ -72,31 +94,31 @@ public class VentanaPrincipal {
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Usuario");
-		lblNewLabel_1.setBounds(110, 78, 91, 14);
+		lblNewLabel_1.setBounds(90, 75, 91, 14);
 		lblNewLabel_1.setForeground(Color.RED);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Correo electronico");
-		lblNewLabel_2.setBounds(110, 109, 112, 14);
+		lblNewLabel_2.setBounds(90, 109, 112, 14);
 		lblNewLabel_2.setForeground(Color.RED);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Contraseña");
-		lblNewLabel_3.setBounds(110, 140, 112, 14);
+		lblNewLabel_3.setBounds(90, 140, 112, 14);
 		lblNewLabel_3.setForeground(Color.RED);
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Repite Contraseña");
-		lblNewLabel_4.setBounds(110, 171, 112, 14);
+		lblNewLabel_4.setBounds(90, 172, 112, 14);
 		lblNewLabel_4.setForeground(Color.RED);
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_4);
 		
 		JButton btnNewButton = new JButton("Conectate");
-		btnNewButton.setBounds(201, 288, 89, 23);
+		btnNewButton.setBounds(188, 317, 102, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Login frame = new Login();
@@ -108,7 +130,7 @@ public class VentanaPrincipal {
 		frmRegistroBetaDiablo.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Registrate");
-		btnNewButton_1.setBounds(201, 233, 89, 23);
+		btnNewButton_1.setBounds(188, 258, 102, 23);
 		btnNewButton_1.setForeground(Color.BLACK);
 		btnNewButton_1.setBackground(Color.RED);
 		btnNewButton_1.addActionListener(new ActionListener() {			
@@ -122,26 +144,36 @@ public class VentanaPrincipal {
 					frame.setVisible(true);
 					frmRegistroBetaDiablo.setVisible(false);	
 				}else {
-					if(!passwordField.getText().equals(passwordField_1.getText())) {
-						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Contraseña no coincide","ERROR",JOptionPane.WARNING_MESSAGE);
+					if(txtUsuario.getText().isEmpty() && txtExamplemailcom.getText().isEmpty() && passwordField.getText().isEmpty() && passwordField_1.getText().isEmpty()) {
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Por favor, intorduce los datos del formulario","Formulario vacio",JOptionPane.WARNING_MESSAGE);
+						passwordField.setBackground(Color.RED);
+						passwordField.setForeground(Color.BLACK);
+						passwordField_1.setBackground(Color.RED);
+						passwordField_1.setForeground(Color.BLACK);
+						txtUsuario.setForeground(Color.BLACK);
+						txtUsuario.setBackground(Color.RED);
+						txtExamplemailcom.setBackground(Color.RED);
+						txtExamplemailcom.setForeground(Color.BLACK);
+					}else if(!passwordField.getText().equals(passwordField_1.getText())) {
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Introduce la misma contraseña en ambos campos","Contraseña no coincide",JOptionPane.ERROR_MESSAGE);
 						passwordField.setBackground(Color.RED);
 						passwordField.setForeground(Color.BLACK);
 						passwordField_1.setBackground(Color.RED);
 						passwordField_1.setForeground(Color.BLACK);
 					}else if(txtUsuario.getText().isEmpty()) {
-						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Usuario vacio","ERROR",JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Introduce un usuario","Usuario vacio",JOptionPane.WARNING_MESSAGE);
 						txtUsuario.setForeground(Color.BLACK);
 						txtUsuario.setBackground(Color.RED);
 					}else if(txtExamplemailcom.getText().isEmpty()){
-						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Email vacio","ERROR",JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Introduce un correo","Email vacio",JOptionPane.WARNING_MESSAGE);
 						txtExamplemailcom.setBackground(Color.RED);
 						txtExamplemailcom.setForeground(Color.BLACK);
 					}else if(!txtExamplemailcom.getText().contains("@") && !txtExamplemailcom.getText().contains(".")){
-						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Email no válido","ERROR",JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Introduce un email que sea válido","Email no válido",JOptionPane.ERROR_MESSAGE);
 						txtExamplemailcom.setBackground(Color.RED);
 						txtExamplemailcom.setForeground(Color.BLACK);
 					}else {
-						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Campo de contraseña vacio","ERROR",JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(frmRegistroBetaDiablo,"Introduce una contraseña","Contraseña vacia",JOptionPane.WARNING_MESSAGE);
 						passwordField.setBackground(Color.RED);
 						passwordField.setForeground(Color.BLACK);
 						passwordField_1.setBackground(Color.RED);
@@ -155,7 +187,7 @@ public class VentanaPrincipal {
 		frmRegistroBetaDiablo.getContentPane().add(btnNewButton_1);
 		
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(296, 75, 133, 20);
+		txtUsuario.setBounds(272, 72, 156, 20);
 		txtUsuario.setForeground(Color.RED);
 		txtUsuario.setBackground(Color.BLACK);
 		txtUsuario.setFont(new Font("Tahoma", Font.ITALIC, 11));
@@ -163,7 +195,7 @@ public class VentanaPrincipal {
 		txtUsuario.setColumns(10);
 		
 		txtExamplemailcom = new JTextField();
-		txtExamplemailcom.setBounds(296, 106, 133, 20);
+		txtExamplemailcom.setBounds(272, 106, 156, 20);
 		txtExamplemailcom.setBackground(Color.BLACK);
 		txtExamplemailcom.setForeground(Color.RED);
 		txtExamplemailcom.setFont(new Font("Tahoma", Font.ITALIC, 11));
@@ -171,29 +203,27 @@ public class VentanaPrincipal {
 		txtExamplemailcom.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(296, 168, 133, 20);
+		passwordField.setBounds(272, 169, 156, 20);
 		passwordField.setBackground(Color.BLACK);
 		passwordField.setForeground(Color.RED);
 		frmRegistroBetaDiablo.getContentPane().add(passwordField);
 		
 		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(296, 137, 133, 20);
+		passwordField_1.setBounds(272, 137, 156, 20);
 		passwordField_1.setBackground(Color.BLACK);
 		passwordField_1.setForeground(Color.RED);
 		frmRegistroBetaDiablo.getContentPane().add(passwordField_1);
 		
 		JLabel lblNewLabel_5 = new JLabel("¿Ya tienes una cuenta?");
-		lblNewLabel_5.setBounds(188, 263, 121, 14);
+		lblNewLabel_5.setBounds(169, 292, 156, 14);
 		lblNewLabel_5.setForeground(Color.RED);
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setForeground(Color.RED);
+		lblNewLabel_6.setBackground(new Color(240, 240, 240));
 		lblNewLabel_6.setBounds(0, 0, 495, 511);
 		lblNewLabel_6.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/img/Diablo IV.jpg")));
 		frmRegistroBetaDiablo.getContentPane().add(lblNewLabel_6);
-		
-		JRadioButton radioButton = new JRadioButton("New radio button");
-		radioButton.setBounds(296, 196, 109, 23);
-		frmRegistroBetaDiablo.getContentPane().add(radioButton);
 	}
 }
